a566876c46fcd1b97293459dfac60108ed1d6eea
Eduardo Flores <edfloreshz@gmail.com>
2022-08-05 21:56:40 -0700

    Fetch students implemented

--- Flutter.Client/lib/models/alumno.dart
+++ Flutter.Client/lib/models/alumno.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
 class Alumno {
   int idAlumno;
   String nombres;
@@ -12,5 +18,27 @@ class Alumno {
   factory Alumno.fromJson(Map<String, dynamic> json) {
     return Alumno(json['idAlumno'], json['nombres'], json['apellidoPaterno'],
         json['apellidoMaterno'], json['curp'], json['activo']);
+  }
+
+  static Future<List<Alumno>> fetchAlumnos() async {
+    const storage = FlutterSecureStorage();
+    var token = await storage.read(key: 'token');
+    final response = await http.get(
+      Uri.parse('https://localhost:5000/api/alumno'),
+      headers: {HttpHeaders.authorizationHeader: 'Bearer $token'},
+    );
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      var jsonResponse = json.decode(response.body);
+      return jsonResponse
+          .map<Alumno>((alumno) => Alumno.fromJson(alumno))
+          .toList();
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('No se pudo obtener la lista de alumnos');
+    }
   }
 }
--- Flutter.Client/lib/pages/alumnos.dart
+++ Flutter.Client/lib/pages/alumnos.dart
@@ -1,12 +1,53 @@
-import 'package:flutter/cupertino.dart';
+import 'package:academic/models/alumno.dart';
+import 'package:flutter/material.dart';
 
-class Alumnos extends StatelessWidget {
+class Alumnos extends StatefulWidget {
   const Alumnos({Key? key}) : super(key: key);
 
   @override
+  State<Alumnos> createState() => _AlumnosState();
+}
+
+class _AlumnosState extends State<Alumnos> {
+  late Future<List<Alumno>> futureAlumnos;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlumnos = Alumno.fetchAlumnos();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Alumnos"),
+    return Expanded(
+      child: FutureBuilder<List<Alumno>>(
+        future: futureAlumnos,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var alumnos = snapshot.data!;
+            return alumnos.isNotEmpty
+                ? ListView.builder(
+                    itemCount: alumnos.length,
+                    itemBuilder: (context, index) {
+                      var alumno = alumnos[index];
+                      return ListTile(
+                        title: Card(
+                          child: Row(children: [
+                            Text(
+                                '${alumno.nombres} ${alumno.apellidoPaterno} ${alumno.apellidoMaterno}'),
+                            Text(alumno.curp),
+                          ]),
+                        ),
+                      );
+                    },
+                  )
+                : const Center(child: Text('No items'));
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          return const CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
